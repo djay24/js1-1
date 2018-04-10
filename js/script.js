a70d6213bf38cc1e797cac51d020b6cbcce6bc95
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-04-09 20:59:31 -0700

    Worked on vars and alerts and confirm

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var myHistory = 'I want to start a new career coding either for web or mobile.';
+// console.log (myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
+
+// window .confirm (myHistory);
