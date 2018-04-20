ebe8984c953db99a3bd3086637debf5082209e9a
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-04-19 20:31:06 -0700

    push to leave class

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 /*jshint esversion: 6 */
 
-let firstName = prompt("Answer Question 1 of 3\n\n\nWhats your First Name");
+let birthday = new Date(prompt('Thank you for participating in my survey.\n\n Please enter your birth date', 'mm/dd/yyyy'));
+year = birthday.getFullYear();
+console.log(year);
 
-let lastName = prompt("Answer Question 2 of 3\n\n\nWhats your Last Name");
+let age = (2018 - year);
+console.log(age);
+//   if (age == null){
+//   return false;
+// }
+  if (age < 18) {
+  alert('I am so sorry you are not old enough to participate in this survey.');
+} else if (age > 79) {
+  alert('I am so sorry you are to old for this survey, but here what was happening  around 80 years ago.\n\nIn 1934, 80 years before 2014, the world was in the Great Depression, Hitler named himself Fuehrer of Germany, Mussolini was Prime Minister of Italy and Roosevelt was President of the United States. The continuing Midwest drought destroyed 35 million acres of farmland in Oklahoma, and President Roosevelt instituted public works programs to build roads, dams and bridges to create jobs. Unemployment was at 22 percent.');
+} else {
+  let rockBand = prompt('You are the perfect age for my survey!\n\nThe one and only question I for you is\n\nWhats the greatest rock band of all times?');
 
-console.log (firstName);
-alert(lastName);
+  console.log(rockBand.toLowerCase());
 
-let birthDay = prompt("Answer Question 3 of 3\n\n\nWhen were you Born\nPlease use mm/dd/yyyy");
-
-console.log (birthDay);
-alert (birthDay = new Date(birthDay));
+  if (rockBand.toLowerCase() == "led zeppelin", "led zepplin") {
+    alert(`HATS OFF TO YOU MY FRIEND!\n\nLED ZEPPELIN IS THE GREATEST BAND EVER!`);
+    window.location.href = 'http://www.ledzeppelin.com';
+  } else {
+    alert(`YOU MUST BE EXTREMELY DELUSIONAL!\n\n${rockBand.toUpperCase()} IS A MUSICAL JOKE!\n\nLED ZEPPELIN IS THE GREATEST BAND EVER!`);
+    window.location.href = 'http://www.ledzeppelin.com';
+  }
+}
