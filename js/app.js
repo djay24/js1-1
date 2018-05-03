f599b656e9b919a500499e14d73b736a47039c8c
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-05-02 20:56:00 -0700

    end class

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
 /*jshint esversion: 6 */
+console.log (document.forms.login.username.value);
 
-let firstName = prompt("Answer Question 1 of 3\n\n\nWhats your First Name");
-
-let lastName = prompt("Answer Question 2 of 3\n\n\nWhats your Last Name");
-
-console.log (firstName);
-alert(lastName);
-
-let birthDay = prompt("Answer Question 3 of 3\n\n\nWhen were you Born\nPlease use mm/dd/yyyy");
-
-console.log (birthDay);
-alert (birthDay = new Date(birthDay));
+document.forms.login.username.addEventListener ('click', e => {
+  e.preventDefault();
+  console.log(e.target.value)
+})
