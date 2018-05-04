a7f6e5ab62efebdb0cfb3d6ab6008de13cef808b
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-05-04 16:44:59 -0700

    Setup links for html add back ground and started form

--- js/app.js
+++ js/app.js
@@ -1,13 +1,1 @@
 /*jshint esversion: 6 */
-
-let firstName = prompt("Answer Question 1 of 3\n\n\nWhats your First Name");
-
-let lastName = prompt("Answer Question 2 of 3\n\n\nWhats your Last Name");
-
-console.log (firstName);
-alert(lastName);
-
-let birthDay = prompt("Answer Question 3 of 3\n\n\nWhen were you Born\nPlease use mm/dd/yyyy");
-
-console.log (birthDay);
-alert (birthDay = new Date(birthDay));
