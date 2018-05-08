5bd65fa6d2fa154fe654832067beb88941f7053c
Jeff <you@example.com>
2018-05-07 17:19:49 -0700

    Started JS get info from form and insert to ul

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 /*jshint esversion: 6 */
+
+
+
+document.getElementById('submit').addEventListener('click', function ()  {
+  let day = document.getElementById('day').value;
+  let time = document.getElementById('time').value;
+  let todo = document.getElementById('comment').value;
+  todoItem = [day, time, todo];
+  str = todoItem.toString();
+  if (str) addItemTodo(str);
+
+  // console.log(todoItem);
+  // console.log(str);
+  // console.log(todo);
+  });
+
+function addItemTodo (listItem) {
+  let list = document.getElementById('Monday');
+  let str = document.createElement('li');
+  list.innerText = listItem;
+  console.log(listItem);
+  list.appendChild(str);
+}
