9bec858bfdcbd19debdc86097ddc24b7145678c8
Jeff <you@example.com>
2018-05-10 22:01:04 -0700

    add remove and complet buttons and fix margins

--- js/app.js
+++ js/app.js
@@ -7,18 +7,37 @@ document.getElementById('submit').addEventListener('click', function (e)  {
   let day = document.getElementById('day').value;
   let time = document.getElementById('time').value;
   let todo = document.getElementById('comment').value;
-  let todoItem = `${time} ${todo}`
+  let todoItem = `${time} ${todo}`;
   console.log(todoItem);
-  // todoItem = [time, todo];
-  // str = todoItem.toString();
+
   if (todoItem) addItemTodo(todoItem);
   document.getElementById('comment').value = "";
 
   });
 
 function addItemTodo (listItem) {
+  //Get ul  and create li
   let listTime = document.getElementById('Monday');
   let listComment = document.createElement('li');
   listComment.innerText = listItem;
-  listTime.appendChild(listComment)
+
+  //Add Font Awesome li Buttons
+  deleteIcon = '<i class="fas fa-trash-alt"></i>';
+  completeIcon = '<i class="fas fa-check-circle"></i>';
+
+  let buttonBox = document.createElement('div');
+  buttonBox.classList.add("buttonBox");
+
+  let remove = document.createElement("button");
+  remove.classList.add('remove');
+  remove.innerHTML = deleteIcon;
+
+  let complete = document.createElement("button");
+  complete.classList.add('complete');
+  complete.innerHTML = completeIcon;
+
+  buttonBox.appendChild(complete);
+  buttonBox.appendChild(remove);
+  listComment.appendChild(buttonBox);
+  listTime.appendChild(listComment);
 }
