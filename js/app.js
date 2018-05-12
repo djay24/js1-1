080d8caafc77c7d17ef8352eda037af2ee0cb7f2
Jeff <you@example.com>
2018-05-12 14:52:24 -0700

    remove buttons on completed box

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ document.getElementById('submit').addEventListener('click', function(e) {
   let day = document.getElementById('day').value;
   let time = document.getElementById('time').value;
   let todo = document.getElementById('comment').value;
-  let todoItem = `${time} ${todo}`;
+  let todoItem = `${time} \u2002 ${todo}`;
   console.log(todoItem);
 
   if (todoItem) addItemTodo(todoItem);
@@ -21,6 +21,7 @@ function addItemTodo(listItem) {
   let listComment = document.createElement('li');
   listComment.innerText = listItem;
   listComment.classList.add('li-time');
+  listComment.classList.add('li-completed');
 
 
   //Add Font Awesome li Buttons
@@ -34,11 +35,15 @@ function addItemTodo(listItem) {
   remove.classList.add('remove');
   remove.innerHTML = deleteIcon;
 
+
+  let completeBox = document.createElement('div');
+  listBox.classList.add("listBox");
+
   let complete = document.createElement("button");
   complete.classList.add('complete');
   complete.innerHTML = completeIcon;
 
-  listBox.appendChild(complete);
+  completeBox.appendChild(complete);
   listBox.appendChild(remove);
   listComment.appendChild(listBox);
   listTime.appendChild(listComment);
@@ -56,20 +61,22 @@ function addItemTodo(listItem) {
 
   // Complete Button Function
   complete.addEventListener('click', completeTodo);
-  // document.getElementById('listItem');
   function completeTodo() {
-    let moveComplete = listBox.parentNode;
+    let moveComplete = listBox.parentNode;//leave alone
+    let removeIcon = listBox;
+    let ulComplete = document.getElementById('ulComplete');
     ulComplete.appendChild(moveComplete);
-    moveComplete.classList.add(complete-li-box);
-  }
+    moveComplete.removeChild(removeIcon);
+    // listBox.classList.add("listBox");
+    // remove.classList.add('remove');
+    // remove.innerHTML = deleteIcon;
+    // listBox.appendChild(remove);
+    console.log(removeIcon);
+    }
 
+    // function removeElement(){
+    //   removeIcon.parentNode.removeChild(complete);
+    //
+    // }
 
 }
-
-
-// Delete Todo item
-// function deleteTodo() {
-//   let listItem = this.parentNode.parentNode;
-//   let parent = listItem.parentNode;
-//   parent.removeChild(listItem);
-// }
